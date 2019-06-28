76ada28508494cf1bc1b09c252354d6db0495408
Kevin Nhan <kbn.nhan@gmail.com>
2019-06-28 19:47:31 -0400

    Re-did json and read method

--- src/gsontest/GsonTest.java
+++ src/gsontest/GsonTest.java
@@ -7,9 +7,12 @@ package gsontest;
 
 import com.google.gson.Gson;
 
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 /**
  *
  * @author kevinnhan
@@ -21,17 +24,23 @@ public class GsonTest {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-        String path = "/Users/kevinnhan/Desktop/GsonTest/src/gsontest/question1.json";
+        String path = "/Users/kevinnhan/Desktop/GsonTest/src/gsontest/assessment1.json";
         
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
 
         Gson gson = new Gson();
-        MultiChoice json = gson.fromJson(bufferedReader, MultiChoice.class);
+        Assessment json = gson.fromJson(bufferedReader, Assessment.class);
 
         System.out.println(json.getClass());
         System.out.println(json.toString());
-        System.out.println(json.text);
-        System.out.println(json.getAnswer());
+        System.out.println(json.title);
+        
+        //String converted = String.valueOf(json.questions[0]);
+        //System.out.println(converted);
+        
+        //MultiChoice question1 = gson.fromJson(converted, MultiChoice.class);
+        //System.out.println(question1.text);
+
     }
     
 }
